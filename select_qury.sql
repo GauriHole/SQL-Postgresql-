12cb3c9fb6ea28a2c8c596d83ab151ba4c973c90
Gauri Rajendra Hole <gaurihole2502@gmail.com>
2024-09-04 11:43:18 +0530

    Add files via upload

---
+++ select_qury.sql
@@ -0,0 +1,9 @@
+/*
+select * from city
+select * from actor
+select last_name, first_name from actor;
+select first_name, last_name from actor;
+select first_name,last_name,email from customer
+select distinct release_year from film;
+select distinct rating from film;
+*/
